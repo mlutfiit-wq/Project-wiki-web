f6d0242a794c33c0f8ae8ffa34aae30b02e10c12
mlutfiit-wq <m.lutfi.it@gmail.com>
2026-07-05 20:20:08 +0800

    Delete project-remastred directory

--- project-remastred/asset/js/main.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const navContainer = document.getElementById('navContainer');
-
-menuToggle.addEventListener('click', () => {
-  navContainer.classList.toggle('active');
-});
-
-const track = document.querySelector('.carousel-track')
